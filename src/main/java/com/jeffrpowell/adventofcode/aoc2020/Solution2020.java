d36d1600ce44324b425dc7e7e6ca89ac1155d0f6
jeffrpowell <jeffr.powell@outlook.com>
2020-11-21 05:50:14 -0700

    prep for 2020 solutions

---
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2020/Solution2020.java
@@ -0,0 +1,11 @@
+package com.jeffrpowell.adventofcode.aoc2020;
+
+import com.jeffrpowell.adventofcode.Solution;
+
+public abstract class Solution2020<T> extends Solution<T>
+{
+	@Override
+	public int getYear() {
+		return 2020;
+	}
+}
