c38c271acaeb297a17eefea321660f3ef247c3cb
jeffrpowell <jeffr.powell@outlook.com>
2020-12-19 06:08:01 -0700

    2020 day 19 part 1 (and part 2 progress)

---
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2020/Day19.java
@@ -0,0 +1,203 @@
+package com.jeffrpowell.adventofcode.aoc2020;
+
+import com.jeffrpowell.adventofcode.inputparser.InputParser;
+import com.jeffrpowell.adventofcode.inputparser.InputParserFactory;
+import com.jeffrpowell.adventofcode.inputparser.rule.Rule;
+import com.jeffrpowell.adventofcode.inputparser.rule.RuleListUtil;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+
+public class Day19 extends Solution2020<Rule>{
+
+    @Override
+    public int getDay() {
+        return 19;
+    }
+
+    @Override
+    public InputParser<Rule> getInputParser() {
+        return InputParserFactory.getRuleParser("\n", Map.of(
+            "letter", Pattern.compile("(\\d+): \"(\\w)\""),
+            "concat", Pattern.compile("(\\d+): (\\d+) (\\d+)$"),
+            "baton", Pattern.compile("(\\d+): (\\d+)$"),
+            "or", Pattern.compile("(\\d+): (\\d+) \\| (\\d+)"),
+            "doubleor", Pattern.compile("(\\d+): (\\d+) (\\d+) \\| (\\d+) (\\d+)"),
+            "message", Pattern.compile("(\\w+)")
+        ));
+    }
+
+    @Override
+    protected String part1(List<Rule> input) {
+        Map<String, List<Rule>> groupedRules = RuleListUtil.groupByRulePatternKey(input);
+        Map<Integer, Function<String, String>> rules = new HashMap<>();
+        for (Rule rule : groupedRules.get("letter")) {
+            rules.put(rule.getInt(0), message -> message.startsWith(rule.getString(1)) ? message.substring(1) : "x");
+        }
+        for (Rule rule : groupedRules.get("baton")) {
+            int innerRuleI = rule.getInt(1);
+            rules.put(rule.getInt(0), message -> {
+                Function<String, String> innerRule = rules.get(innerRuleI);
+                return innerRule.apply(message);
+            });
+        }
+        for (Rule rule : groupedRules.get("concat")) {
+            int left = rule.getInt(1);
+            int right = rule.getInt(2);
+            rules.put(rule.getInt(0), message -> {
+                Function<String, String> leftRule = rules.get(left);
+                Function<String, String> rightRule = rules.get(right);
+                return rightRule.apply(leftRule.apply(message));
+            });
+        }
+        for (Rule rule : groupedRules.get("or")) {
+            int left = rule.getInt(1);
+            int right = rule.getInt(2);
+            rules.put(rule.getInt(0), message -> {
+                Function<String, String> leftRule = rules.get(left);
+                String result1 = leftRule.apply(message);
+                if (!result1.contains("x")) {
+                    return result1;
+                }
+                Function<String, String> rightRule = rules.get(right);
+                String result2 = rightRule.apply(message);
+                if (!result2.contains("x")) {
+                    return result2;
+                }
+                return "x";
+            });
+        }
+        for (Rule rule : groupedRules.get("doubleor")) {
+            int left1 = rule.getInt(1);
+            int right1 = rule.getInt(2);
+            int left2 = rule.getInt(3);
+            int right2 = rule.getInt(4);
+            rules.put(rule.getInt(0), message -> {
+                Function<String, String> left1Rule = rules.get(left1);
+                Function<String, String> right1Rule = rules.get(right1);
+                String result1 = right1Rule.apply(left1Rule.apply(message));
+                if (!result1.contains("x")) {
+                    return result1;
+                }
+                Function<String, String> left2Rule = rules.get(left2);
+                Function<String, String> right2Rule = rules.get(right2);
+                String result2 = right2Rule.apply(left2Rule.apply(message));
+                if (!result2.contains("x")) {
+                    return result2;
+                }
+                return "x";
+            });
+        }
+        return Long.toString(groupedRules.get("message").stream()
+            .map(rule -> rules.get(0).apply(rule.getString(0)).isEmpty())
+            .filter(b -> b)
+            .count());
+    }
+
+    @Override
+    protected String part2(List<Rule> input) {
+        Map<String, List<Rule>> groupedRules = RuleListUtil.groupByRulePatternKey(input);
+        Map<Integer, Function<String, String>> rules = new HashMap<>();
+        for (Rule rule : groupedRules.get("letter")) {
+            rules.put(rule.getInt(0), message -> message.startsWith(rule.getString(1)) ? message.substring(1) : "x");
+        }
+        for (Rule rule : groupedRules.get("baton")) {
+            if (rule.getInt(0) == 8) {
+                //hard-coded to 8: 42 | 42 8
+                rules.put(8, message -> {
+                    Function<String, String> plain = rules.get(42);
+                    Function<String, String> loop = rules.get(8);
+                    String tryLoop = loop.apply(plain.apply(message));
+                    if (!tryLoop.contains("x")) {
+                        return tryLoop;
+                    }
+                    String endLoop = plain.apply(message);
+                    if (!endLoop.contains("x")) {
+                        return endLoop;
+                    }
+                    return "x";
+                });
+            }
+            else {
+                int innerRuleI = rule.getInt(1);
+                rules.put(rule.getInt(0), message -> {
+                    Function<String, String> innerRule = rules.get(innerRuleI);
+                    return innerRule.apply(message);
+                });
+            }
+        }
+        for (Rule rule : groupedRules.get("concat")) {
+            int left = rule.getInt(1);
+            int right = rule.getInt(2);
+            rules.put(rule.getInt(0), message -> {
+                Function<String, String> leftRule = rules.get(left);
+                Function<String, String> rightRule = rules.get(right);
+                return rightRule.apply(leftRule.apply(message));
+            });
+        }
+        for (Rule rule : groupedRules.get("or")) {
+            if (rule.getInt(0) == 11) {
+                //hard-coded to 11: 42 31 | 42 11 31
+                rules.put(11, message -> {
+                    Function<String, String> left = rules.get(42);
+                    Function<String, String> loop = rules.get(11);
+                    Function<String, String> right = rules.get(31);
+                    String tryLoop = right.apply(loop.apply(left.apply(message)));
+                    if (!tryLoop.contains("x")) {
+                        return tryLoop;
+                    }
+                    String endLoop = right.apply(left.apply(message));
+                    if (!endLoop.contains("x")) {
+                        return endLoop;
+                    }
+                    return "x";
+                });
+            }
+            else {
+                int left = rule.getInt(1);
+                int right = rule.getInt(2);
+                rules.put(rule.getInt(0), message -> {
+                    Function<String, String> leftRule = rules.get(left);
+                    String result1 = leftRule.apply(message);
+                    if (!result1.contains("x")) {
+                        return result1;
+                    }
+                    Function<String, String> rightRule = rules.get(right);
+                    String result2 = rightRule.apply(message);
+                    if (!result2.contains("x")) {
+                        return result2;
+                    }
+                    return "x";
+                });
+            }
+        }
+        for (Rule rule : groupedRules.get("doubleor")) {
+            int left1 = rule.getInt(1);
+            int right1 = rule.getInt(2);
+            int left2 = rule.getInt(3);
+            int right2 = rule.getInt(4);
+            rules.put(rule.getInt(0), message -> {
+                Function<String, String> left1Rule = rules.get(left1);
+                Function<String, String> right1Rule = rules.get(right1);
+                String result1 = right1Rule.apply(left1Rule.apply(message));
+                if (!result1.contains("x")) {
+                    return result1;
+                }
+                Function<String, String> left2Rule = rules.get(left2);
+                Function<String, String> right2Rule = rules.get(right2);
+                String result2 = right2Rule.apply(left2Rule.apply(message));
+                if (!result2.contains("x")) {
+                    return result2;
+                }
+                return "x";
+            });
+        }
+        return Long.toString(groupedRules.get("message").stream()
+            .map(rule -> rules.get(0).apply(rule.getString(0)).isEmpty())
+            .filter(b -> b)
+            .count());
+    }
+
+}
--- src/main/java/com/jeffrpowell/adventofcode/inputparser/rule/RuleParserJob.java
+++ src/main/java/com/jeffrpowell/adventofcode/inputparser/rule/RuleParserJob.java
@@ -1,18 +1,22 @@
 package com.jeffrpowell.adventofcode.inputparser.rule;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class RuleParserJob implements Callable<Rule>{
     
     private final Integer sortKey;
     private final String input;
     private final Map<String, Pattern> rulePatterns;
+    private static final Logger log = LogManager.getLogger(RuleParserJob.class);
 
     public RuleParserJob(Integer sortKey, String input, Map<String, Pattern> rulePatterns) {
         this.sortKey = sortKey;
@@ -22,21 +26,27 @@ public class RuleParserJob implements Callable<Rule>{
 
     @Override
     public Rule call() throws Exception {
-        Map<String, Matcher> matchers = rulePatterns.entrySet().stream()
-            .collect(Collectors.toMap(
-                Map.Entry::getKey,
-                entry -> entry.getValue().matcher(input)
-            ));
-        
-        Map.Entry<String, Matcher> m = matchers.entrySet().stream()
-            .filter(entry -> entry.getValue().matches())
-            .findAny().get();
-            
-        List<String> tokens = new ArrayList<>();
-        for (int i = 1; i < m.getValue().groupCount() + 1; i++) {
-            tokens.add(m.getValue().group(i));
+        try {
+            Map<String, Matcher> matchers = rulePatterns.entrySet().stream()
+                .collect(Collectors.toMap(
+                    Map.Entry::getKey,
+                    entry -> entry.getValue().matcher(input)
+                ));
+
+            Map.Entry<String, Matcher> m = matchers.entrySet().stream()
+                .filter(entry -> entry.getValue().matches())
+                .findAny().get();
+
+            List<String> tokens = new ArrayList<>();
+            for (int i = 1; i < m.getValue().groupCount() + 1; i++) {
+                tokens.add(m.getValue().group(i));
+            }
+            return new Rule(tokens, m.getKey(), sortKey);
         }
-        return new Rule(tokens, m.getKey(), sortKey);
+        catch (Exception e) {
+            log.error(sortKey + ": exception in rule parser job while looking at " + input, e);
+        }
+        return new Rule(Collections.emptyList(), "", sortKey);
     }
 
 }
