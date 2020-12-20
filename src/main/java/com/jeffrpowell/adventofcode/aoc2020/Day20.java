6891242495eabd40ce5f9354e1db271cf9312d1a
jeffrpowell <jeffr.powell@outlook.com>
2020-12-19 20:09:15 -0800

    2020 day 20 scaffolding

---
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2020/Day20.java
@@ -0,0 +1,29 @@
+package com.jeffrpowell.adventofcode.aoc2020;
+
+import com.jeffrpowell.adventofcode.inputparser.InputParser;
+import com.jeffrpowell.adventofcode.inputparser.InputParserFactory;
+import java.util.List;
+
+public class Day20 extends Solution2020<String>{
+
+    @Override
+    public int getDay() {
+        return 20;
+    }
+
+    @Override
+    public InputParser<String> getInputParser() {
+        return InputParserFactory.getStringParser();
+    }
+
+    @Override
+    protected String part1(List<String> input) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected String part2(List<String> input) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
