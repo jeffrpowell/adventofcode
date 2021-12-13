fcf0a50f3bc34c1d8fda24d4289e89a39c389998
jeffrpowell <jeffr.powell@outlook.com>
2021-12-13 10:42:46 -0700

    WIP 2021 day 11

---
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2021/Day11.java
@@ -0,0 +1,70 @@
+package com.jeffrpowell.adventofcode.aoc2021;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import com.jeffrpowell.adventofcode.Point2DUtils;
+import com.jeffrpowell.adventofcode.inputparser.InputParser;
+import com.jeffrpowell.adventofcode.inputparser.InputParserFactory;
+
+public class Day11 extends Solution2021<List<Integer>>{
+    Map<Point2D, Integer> grid;
+    int rightBoundary = 0;
+    int bottomBoundary = 0;
+    @Override
+    public int getDay() {
+        return 11;
+    }
+
+    @Override
+    public InputParser<List<Integer>> getInputParser() {
+        return InputParserFactory.getIntegerTokenSVParser("");
+    }
+
+    @Override
+    protected String part1(List<List<Integer>> input) {
+        rightBoundary = input.get(0).size() - 1;
+        bottomBoundary = input.size() - 1;
+        grid = Point2DUtils.generateGrid(0, 0, rightBoundary, bottomBoundary).collect(Collectors.toMap(
+            Function.identity(),
+            pt -> input.get(Double.valueOf(pt.getX()).intValue()).get(Double.valueOf(pt.getY()).intValue())
+        ));
+        long flashes = 0;
+        for (int i = 0; i < 100; i++) {
+            flashes += step();
+        }
+        return Long.toString(flashes);
+    }
+
+    private long step() {
+        grid.keySet().stream().forEach(pt -> grid.compute(pt, (k, v) -> v + 1));
+        while(checkForFlashes()) {}
+        Set<Point2D> flashes = grid.entrySet().stream().filter(entry -> entry.getValue() > 9).map(Map.Entry::getKey).collect(Collectors.toSet());
+        flashes.stream().forEach(pt -> grid.put(pt, 0));
+        return flashes.size();
+    }
+
+    private boolean checkForFlashes() {
+        List<Point2D> changesMade = new ArrayList<>();
+        grid.entrySet().stream()
+            .filter(entry -> entry.getValue() == 10)
+            .map(entry -> Point2DUtils.getBoundedAdjacentPts(entry.getKey(), 0, rightBoundary, bottomBoundary, 0, true, true))
+            .flatMap(Set::stream)
+            .peek(changesMade::add)
+            .forEach(pt -> grid.compute(pt, (k, v) -> v < 10 ? v + 1 : v));
+        return !changesMade.isEmpty();
+    }
+
+    @Override
+    protected String part2(List<List<Integer>> input) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    
+}
