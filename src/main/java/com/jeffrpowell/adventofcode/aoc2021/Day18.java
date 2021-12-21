7c9b27f3fd798c0abe2b6ae2ec745f6648bcfddb
jeffrpowell <jeffr.powell@outlook.com>
2021-12-21 13:17:29 -0700

    WIP 2021 day 18

--- src/main/java/com/jeffrpowell/adventofcode/aoc2021/Day18.java
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2021/Day18.java
@@ -1,7 +1,11 @@
 package com.jeffrpowell.adventofcode.aoc2021;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.jeffrpowell.adventofcode.CharArrayUtils;
 import com.jeffrpowell.adventofcode.inputparser.InputParser;
 import com.jeffrpowell.adventofcode.inputparser.InputParserFactory;
 
@@ -19,7 +23,7 @@ public class Day18 extends Solution2021<String> {
 
     @Override
     protected String part1(List<String> input) {
-        // TODO Auto-generated method stub
+        List<Pair> numbers = input.stream().map(this::parseInput).collect(Collectors.toList());
         return null;
     }
 
@@ -28,5 +32,104 @@ public class Day18 extends Solution2021<String> {
         // TODO Auto-generated method stub
         return null;
     }
+
+    private Pair parseInput(String line) {
+        Deque<Pair.Builder> stack = new ArrayDeque<>();
+        Pair.Builder builderInContext = null;
+        int depth = -1;
+        for (Character c : CharArrayUtils.toList(line.toCharArray())) {
+            if (c == '[') {
+                depth++;
+                if (depth > 0) {
+                    stack.push(builderInContext);
+                }
+                builderInContext = new Pair.Builder();
+                builderInContext.depth(depth);
+            }
+            else if (c >= '0' && c <= '9') {
+                builderInContext.pushNumber(new Num(depth, Integer.parseInt(String.valueOf(c))));
+            }
+            else if (c == ']') {
+                Pair p = builderInContext.build();
+                if (stack.isEmpty()) {
+                    return p;
+                }
+                else {
+                    builderInContext = stack.pop();
+                    builderInContext.pushNumber(p);
+                }
+            }
+        }
+        return null;
+    }
+
+    private static class Num implements Member {
+        private int depth;
+        private int value;
+        public Num(int depth, int value) {
+            this.depth = depth;
+            this.value = value;
+        }
+        @Override
+        public long getMagnitude() {
+            return value;
+        }
+
+        @Override
+        public int getDepth() {
+            return depth;
+        }
+    }
+
+    private static class Pair implements Member {
+        private int depth;
+        private Member left;
+        private Member right;
+        
+        public Pair(int depth, Member left, Member right) {
+            this.depth = depth;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public long getMagnitude() {
+            return 3 * left.getMagnitude() + 2 * right.getMagnitude();
+        }
+        
+        @Override
+        public int getDepth() {
+            return depth;
+        }
+
+        public static class Builder {
+            private Member left;
+            private Member right;
+            private boolean pushLeft;
+            private int depth;
+
+            public void pushNumber(Member member) {
+                if (pushLeft) {
+                    this.left = member;
+                    pushLeft = false;
+                }
+                else {
+                    this.right = member;
+                }
+            }
+
+            public void depth(int depth) {
+                this.depth = depth;
+            }
+
+            public Pair build() {
+                return new Pair(depth, left, right);
+            }
+        }
+    }
     
+    private static interface Member {
+        public long getMagnitude();
+        public int getDepth();
+    }
 }
