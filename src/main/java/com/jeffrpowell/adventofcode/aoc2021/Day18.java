3505a23d4d140a96032b78fd3b6c0ce18ac7f05c
jeffrpowell <jeffr.powell@outlook.com>
2021-12-22 13:04:39 -0700

    WIP 2021 day 18

--- src/main/java/com/jeffrpowell/adventofcode/aoc2021/Day18.java
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2021/Day18.java
@@ -24,7 +24,7 @@ public class Day18 extends Solution2021<String> {
     @Override
     protected String part1(List<String> input) {
         List<Pair> numbers = input.stream().map(this::parseInput).collect(Collectors.toList());
-        return null;
+        return Long.toString(numbers.stream().reduce((acc, next) -> new Pair(acc, next)).get().getMagnitude());
     }
 
     @Override
@@ -33,7 +33,7 @@ public class Day18 extends Solution2021<String> {
         return null;
     }
 
-    private Pair parseInput(String line) {
+    Pair parseInput(String line) {
         Deque<Pair.Builder> stack = new ArrayDeque<>();
         Pair.Builder builderInContext = null;
         int depth = -1;
@@ -55,6 +55,7 @@ public class Day18 extends Solution2021<String> {
                     return p;
                 }
                 else {
+                    depth--;
                     builderInContext = stack.pop();
                     builderInContext.pushNumber(p);
                 }
@@ -63,7 +64,7 @@ public class Day18 extends Solution2021<String> {
         return null;
     }
 
-    private static class Num implements Member {
+    static class Num implements Member {
         private int depth;
         private int value;
         public Num(int depth, int value) {
@@ -79,9 +80,40 @@ public class Day18 extends Solution2021<String> {
         public int getDepth() {
             return depth;
         }
+
+        @Override
+        public void acceptVisitor(Visitor v) {
+            v.visitNum(this);
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + depth;
+            result = prime * result + value;
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Num other = (Num) obj;
+            if (depth != other.depth)
+                return false;
+            if (value != other.value)
+                return false;
+            return true;
+        }
+        
     }
 
-    private static class Pair implements Member {
+    static class Pair implements Member {
         private int depth;
         private Member left;
         private Member right;
@@ -90,6 +122,18 @@ public class Day18 extends Solution2021<String> {
             this.depth = depth;
             this.left = left;
             this.right = right;
+        }
+
+        /**
+         * Addition constructor; automatically reduces
+         * @param left
+         * @param right
+         */
+        public Pair(Member left, Member right) {
+            this.left = left;
+            this.right = right;
+            this.depth = 0;
+            updateDepthsAndReduce();
         }
 
         @Override
@@ -102,11 +146,26 @@ public class Day18 extends Solution2021<String> {
             return depth;
         }
 
+        private void updateDepthsAndReduce() {
+
+        }
+
+        @Override
+        public void acceptVisitor(Visitor v) {
+            v.visitPair(this);
+            left.acceptVisitor(v);
+            right.acceptVisitor(v);
+        }
+
         public static class Builder {
             private Member left;
             private Member right;
             private boolean pushLeft;
             private int depth;
+
+            public Builder() {
+                this.pushLeft = true;
+            }
 
             public void pushNumber(Member member) {
                 if (pushLeft) {
@@ -126,10 +185,82 @@ public class Day18 extends Solution2021<String> {
                 return new Pair(depth, left, right);
             }
         }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + depth;
+            result = prime * result + ((left == null) ? 0 : left.hashCode());
+            result = prime * result + ((right == null) ? 0 : right.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Pair other = (Pair) obj;
+            if (depth != other.depth)
+                return false;
+            if (left == null) {
+                if (other.left != null)
+                    return false;
+            } else if (!left.equals(other.left))
+                return false;
+            if (right == null) {
+                if (other.right != null)
+                    return false;
+            } else if (!right.equals(other.right))
+                return false;
+            return true;
+        }
     }
     
-    private static interface Member {
+    static interface Member {
         public long getMagnitude();
         public int getDepth();
+        public void acceptVisitor(Visitor v);
+    }
+
+    static interface Visitor {
+        public void visitPair(Pair p);
+        public void visitNum(Num n);
+    }
+
+    static class ExplodeChecker implements Visitor {
+
+        @Override
+        public void visitPair(Pair p) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void visitNum(Num n) {
+            // TODO Auto-generated method stub
+            
+        }
+
+    }
+
+    static class SplitChecker implements Visitor {
+
+        @Override
+        public void visitPair(Pair p) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void visitNum(Num n) {
+            // TODO Auto-generated method stub
+            
+        }
+
     }
 }
---
+++ src/test/java/com/jeffrpowell/adventofcode/aoc2021/Day18Test.java
@@ -0,0 +1,98 @@
+package com.jeffrpowell.adventofcode.aoc2021;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Day18Test
+{
+	private Day18 day;
+
+	@BeforeEach
+	public void setUp()
+	{
+		day = new Day18();
+	}
+
+	@Test
+	public void testParseInput_simple()
+	{
+		String input = "[1,2]";
+		Day18.Pair expected = new Day18.Pair(0, new Day18.Num(0, 1), new Day18.Num(0, 2));
+		Day18.Pair actual = day.parseInput(input);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testParseInput_nestedLeftTwoLevels()
+	{
+		String input = "[[[1,2],3],4]";
+		Day18.Pair expected = 
+			new Day18.Pair(0, 
+				new Day18.Pair(1, 
+					new Day18.Pair(2, 
+						new Day18.Num(2, 1), 
+						new Day18.Num(2, 2)
+						),
+					new Day18.Num(1, 3)
+					),
+				new Day18.Num(0, 4)
+			);
+		Day18.Pair actual = day.parseInput(input);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testParseInput_nestedRightTwoLevels()
+	{
+		String input = "[1,[2,[3,4]]]";
+		Day18.Pair expected = 
+			new Day18.Pair(0, 
+				new Day18.Num(0, 1),
+				new Day18.Pair(1, 
+					new Day18.Num(1, 2),
+					new Day18.Pair(2, 
+						new Day18.Num(2, 3), 
+						new Day18.Num(2, 4)
+					)
+				)
+			);
+		Day18.Pair actual = day.parseInput(input);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testParseInput_complex()
+	{
+		String input = "[[2,[[2,8],[3,3]]],[[[1,9],9],6]]";
+		Day18.Pair expected = 
+			new Day18.Pair(0, 
+				new Day18.Pair(1, 
+					new Day18.Num(1, 2),
+					new Day18.Pair(2, 
+						new Day18.Pair(3, 
+							new Day18.Num(3, 2),
+							new Day18.Num(3, 8)
+						),
+						new Day18.Pair(3, 
+							new Day18.Num(3, 3), 
+							new Day18.Num(3, 3)
+						)
+					)
+				),
+				new Day18.Pair(1, 
+					new Day18.Pair(2, 
+						new Day18.Pair(3, 
+							new Day18.Num(3, 1),
+							new Day18.Num(3, 9)
+						),
+						new Day18.Num(2, 9)
+					),
+					new Day18.Num(1, 6)
+				)
+			);
+		Day18.Pair actual = day.parseInput(input);
+		assertEquals(expected, actual);
+	}
+}
