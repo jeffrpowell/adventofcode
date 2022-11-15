28951305b7a02d9694751ff4c3b9c13ead30f25d
jeffrpowell <jeffr.powell@outlook.com>
2022-11-15 06:06:00 -0700

    Adding Solution2022 file

---
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2022/Solution2022.java
@@ -0,0 +1,11 @@
+package com.jeffrpowell.adventofcode.aoc2022;
+
+import com.jeffrpowell.adventofcode.Solution;
+
+public abstract class Solution2022<T> extends Solution<T>
+{
+	@Override
+	public int getYear() {
+		return 2022;
+	}
+}
