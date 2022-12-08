d447849468f45bb8fe8ef203938b9a6207dabc1f
jeffrpowell <jeffr.powell@outlook.com>
2022-12-08 09:49:04 -0700

    2022 Day 8

--- src/main/java/com/jeffrpowell/adventofcode/aoc2022/Day8.java
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2022/Day8.java
@@ -1,9 +1,7 @@
 package com.jeffrpowell.adventofcode.aoc2022;
 
 import java.awt.geom.Point2D;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +9,6 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import com.jeffrpowell.adventofcode.Direction;
 import com.jeffrpowell.adventofcode.Point2DUtils;
 import com.jeffrpowell.adventofcode.inputparser.InputParser;
 import com.jeffrpowell.adventofcode.inputparser.InputParserFactory;
@@ -19,6 +16,8 @@ import com.jeffrpowell.adventofcode.inputparser.InputParserFactory;
 public class Day8 extends Solution2022<List<Integer>>{
     private static final Point2D VECTOR_DOWN = new Point2D.Double(0,1);
     private static final Point2D VECTOR_RIGHT = new Point2D.Double(1,0);
+    private static final Point2D VECTOR_LEFT = new Point2D.Double(-1,0);
+    private static final Point2D VECTOR_UP = new Point2D.Double(0,-1);
     
     @Override
     public int getDay() {
@@ -32,7 +31,6 @@ public class Day8 extends Solution2022<List<Integer>>{
 
     @Override
     protected String part1(List<List<Integer>> input) {
-        Deque<Point2D> q = new ArrayDeque<>();
         int width = input.get(0).size();
         int height = input.size();
         Map<Point2D, Integer> grid = Point2DUtils.generateGrid(0, 0, width, height)
@@ -40,85 +38,94 @@ public class Day8 extends Solution2022<List<Integer>>{
                 Function.identity(),
                 p -> input.get(d2i(p.getY())).get(d2i(p.getX()))
             ));
-        int visibleTrees = width * 2 + height * 2 - 4; //count perimeter
-        Point2DUtils.generateGrid(1, 1, width - 1, height - 1).forEach(q::add);
-        Set<Point2D> visited = new HashSet<>();
-        
-        while (!q.isEmpty()) {
-            Point2D p = q.pop();
-            if (visited.contains(p)) {
-                continue;
-            }
-            VisibleResponse r = isVisible(grid, p, width, height);
-            if (r != VisibleResponse.NOT) {
-                visibleTrees++;
-                List<Point2D> ptsToAdd = new ArrayList<>();
-                switch (r) {
-                    case VERTICAL -> ptsToAdd.add(Point2DUtils.applyVectorToPt(VECTOR_DOWN, p));
-                    case HORIZONTAL -> ptsToAdd.add(Point2DUtils.applyVectorToPt(VECTOR_RIGHT, p));
-                    case BOTH -> {
-                        ptsToAdd.add(Point2DUtils.applyVectorToPt(VECTOR_DOWN, p));
-                        ptsToAdd.add(Point2DUtils.applyVectorToPt(VECTOR_RIGHT, p));
-                    }
+        Set<Point2D> visible = new HashSet<>();
+        for (int x = 0; x < width; x++) {
+            //TOP-DOWN
+            Point2D entry = new Point2D.Double(x, 0);
+            visible.add(entry);
+            int lastBiggestSize = grid.get(entry);
+            entry = Point2DUtils.applyVectorToPt(VECTOR_DOWN, entry);
+            while (entry.getY() < height) {
+                if (grid.get(entry) > lastBiggestSize) {
+                    visible.add(entry);
+                    lastBiggestSize = grid.get(entry);
                 }
-                ptsToAdd.stream()
-                    .filter(pt -> pt.getX() < width - 1 && pt.getY() < height - 1)
-                    .forEach(q::add);
+                entry = Point2DUtils.applyVectorToPt(VECTOR_DOWN, entry);
             }
-            visited.add(p);
+            //BOTTOM-UP
+            entry = new Point2D.Double(x, height - 1);
+            visible.add(entry);
+            lastBiggestSize = grid.get(entry);
+            entry = Point2DUtils.applyVectorToPt(VECTOR_UP, entry);
+            while (entry.getY() > -1) {
+                if (grid.get(entry) > lastBiggestSize) {
+                    visible.add(entry);
+                    lastBiggestSize = grid.get(entry);
+                }
+                entry = Point2DUtils.applyVectorToPt(VECTOR_UP, entry);
+            }
         }
-        return Integer.toString(visibleTrees);
+        for (int y = 0; y < height; y++) {
+            //LEFT-RIGHT
+            Point2D entry = new Point2D.Double(0, y);
+            visible.add(entry);
+            int lastBiggestSize = grid.get(entry);
+            entry = Point2DUtils.applyVectorToPt(VECTOR_RIGHT, entry);
+            while (entry.getX() < width) {
+                if (grid.get(entry) > lastBiggestSize) {
+                    visible.add(entry);
+                    lastBiggestSize = grid.get(entry);
+                }
+                entry = Point2DUtils.applyVectorToPt(VECTOR_RIGHT, entry);
+            }
+            //RIGHT-LEFT
+            entry = new Point2D.Double(width - 1, y);
+            visible.add(entry);
+            lastBiggestSize = grid.get(entry);
+            entry = Point2DUtils.applyVectorToPt(VECTOR_LEFT, entry);
+            while (entry.getX() > -1) {
+                if (grid.get(entry) > lastBiggestSize) {
+                    visible.add(entry);
+                    lastBiggestSize = grid.get(entry);
+                }
+                entry = Point2DUtils.applyVectorToPt(VECTOR_LEFT, entry);
+            }
+        }
+        return Integer.toString(visible.size());
     }
 
     private int d2i(Double d) {
         return d.intValue();
     }
 
-    private enum VisibleResponse {VERTICAL,HORIZONTAL,BOTH,NOT;}
-
-    private VisibleResponse isVisible(Map<Point2D, Integer> grid, Point2D tree, int width, int height) {
-        int treeHeight = grid.get(tree);
-        boolean visibleV = false;
-        if (tree.getY() < height / 2) {
-            visibleV = Point2DUtils.getPointsFromSource(tree, 0, tree.getX(), tree.getY() - 1, tree.getX(), true, false)
-                .get(Direction.UP).stream()
-                .map(grid::get)
-                .allMatch(h -> h < treeHeight);
-        }
-        else {
-            visibleV = Point2DUtils.getPointsFromSource(tree, tree.getY()+1, tree.getX(), height - 1, tree.getX(), true, false)
-                .get(Direction.DOWN).stream()
-                .map(grid::get)
-                .allMatch(h -> h < treeHeight);
-        }
-        boolean visibleH = false;
-        if (tree.getX() < width / 2) {
-            visibleH = Point2DUtils.getPointsFromSource(tree, tree.getY(), tree.getX()-1, tree.getY(), 0, true, false)
-                .get(Direction.LEFT).stream()
-                .map(grid::get)
-                .allMatch(h -> h < treeHeight);
-        }
-        else {
-            visibleH = Point2DUtils.getPointsFromSource(tree, tree.getY(), width - 1, tree.getY(), tree.getX()+1, true, false)
-                .get(Direction.RIGHT).stream()
-                .map(grid::get)
-                .allMatch(h -> h < treeHeight);
-        }
-        if (visibleV && visibleH) {
-            return VisibleResponse.BOTH;
-        }
-        if (visibleV) {
-            return VisibleResponse.VERTICAL;
-        }
-        if (visibleH) {
-            return VisibleResponse.HORIZONTAL;
-        }
-        return VisibleResponse.NOT;
-    }
-
     @Override
     protected String part2(List<List<Integer>> input) {
-        return null;
+        int width = input.get(0).size();
+        int height = input.size();
+        Map<Point2D, Integer> grid = Point2DUtils.generateGrid(0, 0, width, height)
+            .collect(Collectors.toMap(
+                Function.identity(),
+                p -> input.get(d2i(p.getY())).get(d2i(p.getX()))
+            ));
+        return Long.toString(grid.entrySet().stream()
+            .map(e -> calcScenicScore(e, grid, width, height))
+            .max(Comparator.naturalOrder()).get());
     }
 
+    private long calcScenicScore(Map.Entry<Point2D, Integer> tree, Map<Point2D, Integer> grid, int width, int height) {
+        return Point2DUtils.getPointsFromSource(tree.getKey(), 0, width-1, height-1, 0, true, false)
+            .values().stream()
+            .map(listOfPts -> listOfPts.stream().map(grid::get).collect(Collectors.toList()))
+            .map(listOfHeights -> treesICanSee(tree.getValue(), listOfHeights))
+            .collect(Collectors.reducing(1L, Math::multiplyExact));
+    }
+
+    private long treesICanSee(Integer mySize, List<Integer> otherTrees) {
+        for (int i = 0; i < otherTrees.size(); i++) {
+            if (otherTrees.get(i) >= mySize) {
+                return i + 1L;
+            }
+        }
+        return otherTrees.size();
+    }
 }
