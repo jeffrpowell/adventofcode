f366a5ef4b9b483c69794f9a5b8aa64d055eba5a
jeffrpowell <jeffr.powell@outlook.com>
2022-12-20 16:11:37 -0700

    2022 Day 19 WIP

--- src/main/java/com/jeffrpowell/adventofcode/aoc2022/Day19.java
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2022/Day19.java
@@ -1,8 +1,14 @@
 package com.jeffrpowell.adventofcode.aoc2022;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.jeffrpowell.adventofcode.inputparser.InputParser;
 import com.jeffrpowell.adventofcode.inputparser.InputParserFactory;
@@ -22,7 +28,7 @@ public class Day19 extends Solution2022<Rule>{
 
     @Override
     protected String part1(List<Rule> input) {
-        List<Blueprint> blueprints = input.stream().map(
+        return input.stream().map(
             r -> new Blueprint(
                 r.getInt(0),
                 r.getInt(1),
@@ -32,8 +38,9 @@ public class Day19 extends Solution2022<Rule>{
                 r.getInt(5),
                 r.getInt(6)
             )
-        ).collect(Collectors.toList());
-        return null;
+        )
+        .map(Blueprint::quality)
+        .collect(Collectors.reducing(0, Math::addExact)).toString();
     }
 
     @Override
@@ -42,6 +49,7 @@ public class Day19 extends Solution2022<Rule>{
     }
 
     private static class Blueprint {
+        private static final int TIMER = 24;
         int id;
         int oreCost_ore;
         int clayCost_ore;
@@ -49,6 +57,7 @@ public class Day19 extends Solution2022<Rule>{
         int obsidianCost_clay;
         int geodeCost_ore;
         int geodeCost_obsidian;
+        Deque<Sim> stack = new ArrayDeque<>();
         public Blueprint(int id, int oreCost_ore, int clayCost_ore, int obsidianCost_ore, int obsidianCost_clay,
                 int geodeCost_ore, int geodeCost_obsidian) {
             this.id = id;
@@ -59,6 +68,93 @@ public class Day19 extends Solution2022<Rule>{
             this.geodeCost_ore = geodeCost_ore;
             this.geodeCost_obsidian = geodeCost_obsidian;
         }
+        
+        record Sim(
+            int time,
+            int ore, int clay, int obsidian, int geode,
+            int oBot, int cBot, int sBot, int gBot
+        ){}
 
+        public int quality() {
+            System.out.println("Trying blueprint " + id);
+            Set<Sim> visited = new HashSet<>();
+            stack.push(new Sim(TIMER, 0, 0, 0, 0, 1, 0, 0, 0));
+            int max = 0;
+            while (!stack.isEmpty()) {
+                Sim s = stack.pop();
+                visited.add(s);
+                if (s.time == 0) {
+                    if (max < s.geode) {
+                        max = s.geode;
+                    }
+                }
+                else if (s.time > 0 && maxGeodesPossible(s) <= max) {
+                }
+                else {
+                    purchaseBots(s)
+                        .map(this::doHarvest)
+                        .filter(sim -> !visited.contains(sim))
+                        .forEach(stack::push);
+                }
+            }
+            return max * id;
+        }
+
+        private int maxGeodesPossible(Sim s) {
+            return ((s.time * (s.time+1)) / 2)  + s.time*s.gBot + s.geode;
+        }
+
+        private Stream<Sim> purchaseBots(Sim s) {
+            if (s.ore >= geodeCost_ore && s.obsidian >= geodeCost_obsidian) {
+                return Stream.of(new Sim(
+                    s.time,
+                    s.ore - geodeCost_ore, s.clay, s.obsidian - geodeCost_obsidian, s.geode - 1,
+                    s.oBot, s.cBot, s.sBot, s.gBot + 1
+                ));
+            }
+            else if (s.cBot == 0) {
+                if (s.ore >= clayCost_ore) {
+                    return Stream.of(new Sim(
+                        s.time,
+                        s.ore - clayCost_ore, s.clay - 1, s.obsidian, s.geode,
+                        s.oBot, s.cBot + 1, s.sBot, s.gBot
+                    ));
+                }
+                else {
+                    return Stream.of(s);
+                }
+            }
+            else {
+                boolean buildClay = s.ore >= clayCost_ore 
+                    && s.cBot < (obsidianCost_clay / obsidianCost_ore);
+                boolean buildObsidian = s.ore >= obsidianCost_ore && s.clay >= obsidianCost_clay
+                    && s.oBot < (geodeCost_obsidian / geodeCost_ore);
+                List<Sim> sims = new ArrayList<>();
+                sims.add(s);
+                if (buildClay) {
+                    sims.add(new Sim(
+                        s.time,
+                        s.ore - clayCost_ore, s.clay - 1, s.obsidian, s.geode,
+                        s.oBot, s.cBot + 1, s.sBot, s.gBot
+                    ));
+                }
+                if (buildObsidian) {
+                    sims.add(new Sim(
+                        s.time,
+                        s.ore - obsidianCost_ore, s.clay - obsidianCost_clay, s.obsidian - 1, s.geode,
+                        s.oBot, s.cBot, s.sBot + 1, s.gBot
+                    ));
+                }
+                return sims.stream();
+            }
+        }
+
+        private Sim doHarvest(Sim s) {
+            return new Sim(
+                s.time - 1,
+                s.ore + s.oBot, s.clay + s.cBot, s.obsidian + s.sBot, s.geode + s.gBot, 
+                s.oBot, s.cBot, s.sBot, s.gBot
+            );
+        }
     }
 }
