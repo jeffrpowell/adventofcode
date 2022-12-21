3670e7823b2c7475c8119cdb2ff3f8cd9781c1a6
jeffrpowell <jeffr.powell@outlook.com>
2022-12-21 11:54:35 -0700

    Removing unused method in 2022 day 20 and switching from ints to longs

--- src/main/java/com/jeffrpowell/adventofcode/aoc2022/Day20.java
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2022/Day20.java
@@ -1,34 +1,32 @@
 package com.jeffrpowell.adventofcode.aoc2022;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.jeffrpowell.adventofcode.inputparser.InputParser;
 import com.jeffrpowell.adventofcode.inputparser.InputParserFactory;
 
-public class Day20 extends Solution2022<Integer>{
-
+public class Day20 extends Solution2022<Long>{
+    private static final long DECRYPT_KEY = 811589153L;
     @Override
     public int getDay() {
         return 20;
     }
 
     @Override
-    public InputParser<Integer> getInputParser() {
-        return InputParserFactory.getIntegerParser();
+    public InputParser<Long> getInputParser() {
+        return InputParserFactory.getLongParser();
     }
 
     @Override
-    protected String part1(List<Integer> input) {
+    protected String part1(List<Long> input) {
         List<Mover> copy = input.stream().map(n -> new Mover(n, false)).collect(Collectors.toList());
         int size = input.size();
         for (int nums = 0; nums < size; nums++) {
             for (int i = 0; i < size; i++) {
                 Mover m = copy.get(i);
                 if (m.moved()) continue;
-                int newIndex = i + m.num();
+                long newIndex = i + m.num();
                 if (m.num() < 0 && newIndex < 1) {
                     newIndex = size - (Math.abs(newIndex) % size) - 1;
                 }
@@ -36,20 +34,20 @@ public class Day20 extends Solution2022<Integer>{
                     newIndex %= size - 1;
                 }
                 copy.remove(i);
-                copy.add(newIndex, new Mover(m.num(), true));
+                copy.add(Long.valueOf(newIndex).intValue(), new Mover(m.num(), true));
                 break;
             }
         }
-        List<Integer> newList = copy.stream()
+        List<Long> newList = copy.stream()
             .map(Mover::num)
             .collect(Collectors.toList());
-        int start = newList.indexOf(0);
+        int start = newList.indexOf(0L);
         int x = (start + 1000) % size;
         int y = (x + 1000) % size;
         int z = (y + 1000) % size;
-        return Integer.toString(newList.get(x) + newList.get(y) + newList.get(z));
+        return Long.toString(newList.get(x) + newList.get(y) + newList.get(z));
     }
-    record Mover(int num, boolean moved){}
+    record Mover(long num, boolean moved){}
 
     private void printList(List<Mover> copy) {
         for (int i = 0; i < copy.size(); i++) {
@@ -59,49 +57,8 @@ public class Day20 extends Solution2022<Integer>{
         System.out.println();
     }
 
-    private String attempt1(List<Integer> input){
-        Map<Key, Index> indices = new HashMap<>();
-        int size = input.size();
-        for (int i = 0; i < size; i++) {
-            int num = input.get(i);
-            int newIndex = i;
-            if (num < 0) {
-                int vector = num < -size ? -(Math.abs(num) % size) : num;
-                newIndex += vector;
-                if (newIndex < 0) {
-                    newIndex = size + newIndex;
-                }
-            }
-            else {
-                int vector = num > size ? num % size : num;
-                newIndex += vector;
-                if (newIndex >= size) {
-                    newIndex %= size;
-                }
-            }
-            indices.put(new Key(input.get(i), i), new Index(i, newIndex));
-        }
-        List<Integer> newList = indices.entrySet().stream()
-            .sorted((e1, e2) -> {
-                if (e1.getValue().newi() != e2.getValue().newi()) {
-                    return Integer.compare(e1.getValue().newi(), e2.getValue().newi());
-                }
-                return -Integer.compare(e1.getValue().original(), e2.getValue().original());
-            })
-            .map(Map.Entry::getKey)
-            .map(Key::num)
-            .collect(Collectors.toList());
-        int start = newList.indexOf(0);
-        int x = (start + 1000) % size;
-        int y = (x + 1000) % size;
-        int z = (y + 1000) % size;
-        return Integer.toString(newList.get(x) + newList.get(y) + newList.get(z));
-    }
-    record Key(int num, int i){}
-    record Index(int original, int newi){}
-
     @Override
-    protected String part2(List<Integer> input) {
+    protected String part2(List<Long> input) {
         return null;
     }
 
