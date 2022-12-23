5c69abb19a49ffef467243d2082e4cccbc9d04e8
jeffrpowell <jeffr.powell@outlook.com>
2022-12-22 21:10:12 -0700

    2022 Day 22 - walking done

--- src/main/java/com/jeffrpowell/adventofcode/aoc2022/Day22.java
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2022/Day22.java
@@ -129,23 +129,60 @@ public class Day22 extends Solution2022<Section>{
     @Override
     protected String part2(List<Section> input) {
         List<List<String>> map = input.get(0).getInput(InputParserFactory.getTokenSVParser(""));
-        GridParse gridParse;
         try {
-            gridParse = parseGrid(map);
-            Map<Point3D, Boolean> grid = gridParse.grid;
-            Point3D start = gridParse.start;
+            Map<Point3D, Boolean> grid = parseGrid(map);
+            Point3D start = new Point3D(0,0,-1);
+            Point3D vector = new Point3D(1, 0, 0);
             String steps = input.get(1).getInput(InputParserFactory.getStringParser()).get(0);
             List<Integer> walks = Arrays.stream(Pattern.compile("[R|L]").split(steps)).map(Integer::parseInt).collect(Collectors.toList());
             List<String> turns = Arrays.stream(Pattern.compile("\\d+").split(steps)).collect(Collectors.toList());
             turns.remove(0);
+            Position2 p = new Position2(start, vector);
+            List<Position2> visited = new ArrayList<>();
+            visited.add(p);
+            for (int i = 0; i < walks.size(); i++) {
+                p = walk2(p, walks.get(i), grid, visited);
+                if (i < turns.size()) {
+                    Point3D newV = rotateVector(turns.get(i).equals("R"), p);
+                    p = new Position2(p.pt, newV);
+                }
+                visited.add(p);
+            }
+            Point3D center = new Point3D(24.5, 24.5, 24.5);
+            Transform t = new Translate(0, -100)
+                .createConcatenation(new Rotate(180, center.getX(), center.getY(), center.getZ(), Rotate.X_AXIS))
+                .createConcatenation(new Rotate(90, center.getX(), center.getY(), center.getZ(), Rotate.Y_AXIS).createInverse())
+                .createConcatenation( new Translate(-1, 0, 0));
+            Point3D remappedPt = t.transform(p.pt);
+            Point3D remappedV = t.transform(p.v);
+            return p.v.toString();
         } catch (NonInvertibleTransformException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    record GridParse(Map<Point3D, Boolean> grid, Point3D start){}
-    private GridParse parseGrid(List<List<String>> map) throws NonInvertibleTransformException {
+    private Point3D rotateVector(boolean right, Position2 p) throws NonInvertibleTransformException {
+        Point3D axis;
+        if (p.pt.getX() == -1 || p.pt.getX() == 50) {
+            axis = Rotate.X_AXIS;
+        }
+        if (p.pt.getY() == -1 || p.pt.getY() == 50) {
+            axis = Rotate.Y_AXIS;
+        }
+        else {
+            axis = Rotate.Z_AXIS;
+        }
+        Rotate r = new Rotate(90, axis);
+        if (right || (!right && axis == Rotate.Z_AXIS)) {
+            return r.transform(p.v);
+        }
+        else {
+            return r.createInverse().transform(p.v);
+        }
+    }
+
+    private Map<Point3D, Boolean> parseGrid(List<List<String>> map) throws NonInvertibleTransformException {
         Map<Point3D, Boolean> grid = new HashMap<>();
         Rectangle bottom = new Rectangle(50, 0, 50, 50);
         Rectangle right = new Rectangle(100, 0, 50, 50);
@@ -176,6 +213,13 @@ public class Day22 extends Solution2022<Section>{
             .createConcatenation(new Translate(-50, -100))
         );
         box = Point3DUtils.getBoundingBox(topPts.keySet());
+        Map<Point3D, Boolean> leftPts = parseSide(map, left, 
+            new Translate(-1, 0, 0)
+            .createConcatenation(new Rotate(90, center.getX(), center.getY(), center.getZ(), Rotate.Y_AXIS).createInverse())
+            .createConcatenation(new Rotate(180, center.getX(), center.getY(), center.getZ(), Rotate.X_AXIS))
+            .createConcatenation(new Translate(0, -100))
+        );
+        box = Point3DUtils.getBoundingBox(leftPts.keySet());
         Map<Point3D, Boolean> backPts = parseSide(map, back, 
             new Translate(0, 1, 0)
             .createConcatenation(new Rotate(90, center.getX(), center.getY(), center.getZ(), Rotate.Z_AXIS).createInverse())
@@ -184,20 +228,14 @@ public class Day22 extends Solution2022<Section>{
             .createConcatenation(new Translate(0, -150))
         );
         box = Point3DUtils.getBoundingBox(backPts.keySet());
-        Map<Point3D, Boolean> leftPts = parseSide(map, left, 
-            new Translate(-1, 0, 0)
-            .createConcatenation(new Rotate(90, center.getX(), center.getY(), center.getZ(), Rotate.Y_AXIS).createInverse())
-            .createConcatenation(new Rotate(180, center.getX(), center.getY(), center.getZ(), Rotate.X_AXIS))
-            .createConcatenation(new Translate(0, -100))
-        );
-        box = Point3DUtils.getBoundingBox(leftPts.keySet());
         grid.putAll(bottomPts);
         grid.putAll(rightPts);
         grid.putAll(frontPts);
         grid.putAll(topPts);
         grid.putAll(leftPts);
         grid.putAll(backPts);
-        return new GridParse(grid, new Point3D(0,0,-1));
+        box = Point3DUtils.getBoundingBox(grid.keySet());
+        return grid;
     }
 
     private Map<Point3D, Boolean> parseSide(List<List<String>> map, Rectangle rect, Transform transform) {
@@ -221,6 +259,32 @@ public class Day22 extends Solution2022<Section>{
         return d.intValue();
     }
 
-    record Position2(Point2D pt, Direction d){}
-
+    record Position2(Point3D pt, Point3D v){}
+    private Position2 walk2(Position2 p, int distance, Map<Point3D, Boolean> grid, List<Position2> visited) {
+        for (int i = 0; i < distance; i++) {
+            Point3D explore = Point3DUtils.applyVectorToPt(p.v, p.pt);
+            Point3D vector = p.v;
+            if (!grid.containsKey(explore)) {
+                //wrap around
+                Point3D oldExplore = explore;
+                final Position2 oldP = p;
+                explore = Point3DUtils.getAdjacentPts(explore, false).stream()
+                    .filter(pt -> !oldP.pt.equals(pt))
+                    .filter(grid::containsKey)
+                    .findFirst().get();
+                vector = explore.subtract(oldExplore);
+            }
+            if (Boolean.TRUE.equals(grid.get(explore))) {
+                //walk
+                p = new Position2(explore, vector);
+                visited.add(p);
+            }
+            else {
+                //hit wall
+                return p;
+            }
+        }
+        visited.remove(visited.size() - 1);
+        return p;
+    }
 }
