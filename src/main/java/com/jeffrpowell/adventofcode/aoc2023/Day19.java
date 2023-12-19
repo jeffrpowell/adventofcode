57938bef946a97760a810730d5e11a4dea4689d4
Jeff Powell <github@jeffpowell.dev>
2023-12-19 13:12:31 -0700

    WIP 2023 day 19 part 2

--- src/main/java/com/jeffrpowell/adventofcode/aoc2023/Day19.java
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2023/Day19.java
@@ -1,7 +1,9 @@
 package com.jeffrpowell.adventofcode.aoc2023;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -9,7 +11,6 @@ import java.util.stream.Collectors;
 import com.jeffrpowell.adventofcode.inputparser.InputParser;
 import com.jeffrpowell.adventofcode.inputparser.InputParserFactory;
 import com.jeffrpowell.adventofcode.inputparser.SplitPartParser;
-import com.jeffrpowell.adventofcode.inputparser.SplitPartParser.Part;
 import com.jeffrpowell.adventofcode.inputparser.rule.Rule;
 import com.jeffrpowell.adventofcode.inputparser.section.Section;
 import com.jeffrpowell.adventofcode.inputparser.section.SectionSplitStrategyFactory;
@@ -30,7 +31,7 @@ public class Day19 extends Solution2023<Section>{
 
     @Override
     public InputParser<Section> getInputParser() {
-        return InputParserFactory.getSectionParser(SectionSplitStrategyFactory.countNewlines(1, false));
+        return InputParserFactory.getSectionParser(SectionSplitStrategyFactory.emptyLines(1, false));
     }
 
     @Override
@@ -52,9 +53,9 @@ public class Day19 extends Solution2023<Section>{
                 p -> ruleParser.parseInput(List.of(p.secondPart())).stream()
                         .map(r -> {
                             if (r.getRulePatternKey().equals("normal")) {
-                                return new Condition(categoryFnFactory.get(r.getString(0)), r.getString(1).equals(">"), r.getInt(2), r.getString(3));
+                                return new Condition(p.firstPart(), r.getString(0), categoryFnFactory.get(r.getString(0)), r.getString(1).equals(">"), r.getInt(2), r.getString(3));
                             }
-                            return new Condition(categoryFnFactory.get("x"), true, Integer.MIN_VALUE, r.getString(0));
+                            return new Condition(p.firstPart(), "z", categoryFnFactory.get("x"), true, -1, r.getString(0));
                         })
                         .collect(Collectors.toList())
             ));
@@ -69,19 +70,68 @@ public class Day19 extends Solution2023<Section>{
             .reduce(0, Math::addExact));
     }
 
-    record Condition(Function<MachinePart, Integer> categoryFn, boolean isGreaterThan, int condition, String outcome) {
+    @Override
+    protected String part2(List<Section> input) {
+        List<SplitPartParser.Part<String, String>> rules = input.get(0).getInput(
+            InputParserFactory.getSplitPartParser(
+                Pattern.compile("\\{"), 
+                InputParserFactory.getStringParser(), 
+                InputParserFactory.getPreParser(s -> s.substring(0, s.length() - 1),
+                    InputParserFactory.getStringParser()))
+        );
+        InputParser<Rule> ruleParser = InputParserFactory.getRuleParser(",", Map.of(
+            "normal", Pattern.compile("(\\w)([<>])(\\d+):(\\w+)"),
+            "end", Pattern.compile("^([^:<>]+)$")
+        ));
+        Map<String, List<Condition>> workflows = rules.stream()
+            .collect(Collectors.toMap(
+                p -> p.firstPart(),
+                p -> ruleParser.parseInput(List.of(p.secondPart())).stream()
+                        .map(r -> {
+                            if (r.getRulePatternKey().equals("normal")) {
+                                return new Condition(p.firstPart(), r.getString(0), categoryFnFactory.get(r.getString(0)), r.getString(1).equals(">"), r.getInt(2), r.getString(3));
+                            }
+                            return new Condition(p.firstPart(), "z", categoryFnFactory.get("x"), true, -1, r.getString(0));
+                        })
+                        .collect(Collectors.toList())
+            ));
+        Map<String, SuperCondition> superConditions = Map.of(
+            "x", new SuperCondition(0, 4001),
+            "m", new SuperCondition(0, 4001),
+            "a", new SuperCondition(0, 4001),
+            "s", new SuperCondition(0, 4001)
+        );
+        traceConditionsDown("in", new RecursionContext(workflows, superConditions));
+        return Long.toString(superConditions.values().stream()
+            .map(sc -> sc.lessThan - sc.greaterThan)
+            .map(Long::valueOf)
+            .reduce(1L, Math::multiplyExact));
+    }
+
+    record Condition(String workflow, String category, Function<MachinePart, Integer> categoryFn, boolean isGreaterThan, int condition, String outcome) {
         boolean matches(MachinePart p) {
             return isGreaterThan ? 
                 categoryFn.apply(p) > condition :
                 categoryFn.apply(p) < condition;
         }
+
+        boolean lacksCondition() {
+            return condition == -1;
+        }
     }
+
+    record SuperCondition(int greaterThan, int lessThan){}
 
     record MachinePart(int x, int m, int a, int s){
         int rating() {
             return x + m + a + s;
         }
     }
+
+    record RecursionContext(
+        Map<String, List<Condition>> workflows,
+        Map<String, SuperCondition> superConditions
+    ){}
 
     private boolean isAccepted(MachinePart part, Map<String, List<Condition>> conditionsMap) {
         List<Condition> conditions = conditionsMap.get("in");
@@ -99,8 +149,51 @@ public class Day19 extends Solution2023<Section>{
         }
     }
 
-    @Override
-    protected String part2(List<Section> input) {
-        return "";
+    private long traceConditionsDown(String outcome, RecursionContext context) {
+        if (outcome.equals("A")) {
+            return scoreSuperConditions(context.superConditions());
+        }
+        else if (outcome.equals("R")) {
+            return 0;
+        }
+        List<Condition> conditionsForOutcome = context.workflows().get(outcome);
+        Map<String, SuperCondition> oppositeSuperConditions = context.superConditions().entrySet().stream()
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        long combinations = 0;
+        for (Condition condition : conditionsForOutcome) {
+            if (condition.lacksCondition()) {
+                combinations += traceConditionsDown(condition.outcome(), context);
+                continue;
+            }
+            SuperCondition formerSuperCondition = oppositeSuperConditions.get(condition.category());
+            SuperCondition match;
+            SuperCondition opposite;
+            if (condition.isGreaterThan()) {
+                match = new SuperCondition(condition.condition(), formerSuperCondition.lessThan());
+                opposite = new SuperCondition(formerSuperCondition.greaterThan(), condition.condition());
+            }
+            else {
+                match = new SuperCondition(formerSuperCondition.greaterThan(), condition.condition());
+                opposite = new SuperCondition(condition.condition(), formerSuperCondition.lessThan());
+            }
+            Map<String, SuperCondition> matchSuperConditions = oppositeSuperConditions.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            matchSuperConditions.put(condition.category(), match);
+            RecursionContext matchContext = new RecursionContext(context.workflows(), matchSuperConditions);
+            combinations += traceConditionsDown(condition.outcome(), matchContext);
+            oppositeSuperConditions.put(condition.category(), opposite);
+        }
+        return combinations;
+    }
+
+    private long scoreSuperConditions(Map<String, SuperCondition> scs) {
+        if (scs.values().stream()
+                .anyMatch(sc -> sc.lessThan() < sc.greaterThan())) {
+            return 0;
+        }
+        return scs.values().stream()
+            .map(sc -> sc.lessThan - sc.greaterThan - 1)
+            .map(Long::valueOf)
+            .reduce(1L, Math::multiplyExact);
     }
 }
