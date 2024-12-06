a822cc4035ca95b45937f83769b6d540eaeb13c2
Jeff Powell <github@jeffpowell.dev>
2024-12-06 06:52:32 -0700

    2024 day 6 (part 1 solved, WIP part 2)

---
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2024/Day6.java
@@ -0,0 +1,147 @@
+package com.jeffrpowell.adventofcode.aoc2024;
+
+import java.awt.geom.Point2D;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.jeffrpowell.adventofcode.Direction;
+import com.jeffrpowell.adventofcode.Point2DUtils;
+import com.jeffrpowell.adventofcode.inputparser.InputParser;
+import com.jeffrpowell.adventofcode.inputparser.InputParserFactory;
+
+public class Day6 extends Solution2024<List<String>>{
+    private static final StringBuilder log = new StringBuilder();
+
+    @Override
+    public int getDay() {
+        return 6;
+    }
+
+    @Override
+    public InputParser<List<String>> getInputParser() {
+        return InputParserFactory.getTokenSVParser("");
+    }
+
+    @Override
+    protected String part1(List<List<String>> input) {
+        Map<Point2D, String> grid = Point2DUtils.generateGrid(0, 0, input.get(0).size(), input.size())
+            .collect(Collectors.toMap(
+                Function.identity(), 
+                pt -> input.get(d2i(pt.getY())).get(d2i(pt.getX()))
+            ));
+        Point2D topLeft = new Point2D.Double(0, 0);
+        Point2D bottomRight = new Point2D.Double(input.size(), input.get(0).size());
+        Set<Point2D> obstacles = grid.entrySet().stream()
+            .filter(e -> e.getValue().equals("#"))
+            .map(Map.Entry::getKey)
+            .collect(Collectors.toSet());
+        Point2D guard = grid.entrySet().stream()
+            .filter(e -> e.getValue().equals("^"))
+            .map(Map.Entry::getKey)
+            .findFirst().get();
+        Direction d = Direction.UP;
+        Set<Point2D> visited = new HashSet<>();
+        Point2DUtils.BoundingBox bb = new Point2DUtils.BoundingBox(topLeft, bottomRight);
+        while(Point2DUtils.pointInsideBoundary(guard, false, bb)){ 
+            visited.add(guard);
+            Point2D next = Point2DUtils.applyVectorToPt(d.asVector(), guard);
+            if (obstacles.contains(next)) {
+                d = d.rotateRight90();
+                next = Point2DUtils.applyVectorToPt(d.asVector(), guard);
+            }
+            guard = next;
+        }
+        // printGrid(guard, guard, obstacles, visited, bb, Optional.empty());
+        return Integer.toString(visited.size());
+        
+    }
+
+    private void printGrid(Point2D startGuard, Point2D guard, Set<Point2D> obstacles, Set<Point2D> visited, Point2DUtils.BoundingBox bb, Optional<Point2D> newObstacle) {
+        Map<Point2D, String> printVals = new HashMap<>();
+        printVals.put(bb.min(), ".");
+        printVals.put(bb.max(), ".");
+        obstacles.stream().forEach(p -> printVals.put(p, "#"));
+        visited.stream().forEach(p -> printVals.put(p, "*"));
+        newObstacle.ifPresent(p -> printVals.put(p, "O"));
+        printVals.put(startGuard, "^");
+        printVals.put(guard, "G");
+        log.append("\n");
+        log.append(Point2DUtils.pointsToString(printVals, "."));
+    }
+
+    private int d2i(Double d) {
+        return d.intValue();
+    }
+
+    record CacheKey(Point2D p, Direction d){}
+
+    @Override
+    protected String part2(List<List<String>> input) {
+        Map<Point2D, String> grid = Point2DUtils.generateGrid(0, 0, input.get(0).size(), input.size())
+            .collect(Collectors.toMap(
+                Function.identity(), 
+                pt -> input.get(d2i(pt.getY())).get(d2i(pt.getX()))
+            ));
+        Point2D topLeft = new Point2D.Double(0, 0);
+        Point2D bottomRight = new Point2D.Double(input.size(), input.get(0).size());
+        Set<Point2D> obstacles = grid.entrySet().stream()
+            .filter(e -> e.getValue().equals("#"))
+            .map(Map.Entry::getKey)
+            .collect(Collectors.toSet());
+        Point2D startGuard = grid.entrySet().stream()
+            .filter(e -> e.getValue().equals("^"))
+            .map(Map.Entry::getKey)
+            .findFirst().get();
+        Point2D guard = startGuard;
+        Direction d = Direction.UP;
+        Set<CacheKey> visited = new HashSet<>();
+        Point2DUtils.BoundingBox bb = new Point2DUtils.BoundingBox(topLeft, bottomRight);
+        Set<Point2D> loopObstaclePts = new HashSet<>();
+        while(Point2DUtils.pointInsideBoundary(guard, false, bb)){ 
+            Point2D next = Point2DUtils.applyVectorToPt(d.asVector(), guard);
+            if (obstacles.contains(next)) {
+                d = d.rotateRight90();
+                next = Point2DUtils.applyVectorToPt(d.asVector(), guard);
+            }
+            if (!obstacles.contains(next)) {
+                Set<Point2D> simulatedObstacles = Stream.concat(Stream.of(next), obstacles.stream()).collect(Collectors.toSet());
+                Set<CacheKey> visitedCopy = visited.stream().collect(Collectors.toSet());
+                if (obstacleProducesLoop(startGuard, guard, d, visitedCopy, simulatedObstacles, next, bb)) {
+                    loopObstaclePts.add(next);
+                }
+            }
+            visited.add(new CacheKey(guard, d));
+            guard = next;
+        }
+        try {
+            Files.writeString(Path.of("output.txt"), log.toString());
+        } catch (IOException e) {}
+        return Integer.toString(loopObstaclePts.size());
+    }
+
+    private boolean obstacleProducesLoop(Point2D startGuard, Point2D guard, Direction d, Set<CacheKey> visited, Set<Point2D> obstacles, Point2D newObstacle, Point2DUtils.BoundingBox bb) {
+        while(Point2DUtils.pointInsideBoundary(guard, false, bb)){ 
+            if (!visited.add(new CacheKey(guard, d))) {
+                printGrid(startGuard, guard, obstacles, visited.stream().map(CacheKey::p).collect(Collectors.toSet()), bb, Optional.of(newObstacle));
+                return true;
+            };
+            Point2D next = Point2DUtils.applyVectorToPt(d.asVector(), guard);
+            if (obstacles.contains(next)) {
+                d = d.rotateRight90();
+                next = Point2DUtils.applyVectorToPt(d.asVector(), guard);
+            }
+            guard = next;
+        }
+        return false;
+    }
+}
