a01b6b1208a01ae3986dcdb8b327e9ca1122d92d
Jeff Powell <github@jeffpowell.dev>
2024-12-06 14:33:20 -0700

    2017 day 1

---
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2017/Day1.java
@@ -0,0 +1,57 @@
+package com.jeffrpowell.adventofcode.aoc2017;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.jeffrpowell.adventofcode.inputparser.InputParser;
+import com.jeffrpowell.adventofcode.inputparser.InputParserFactory;
+
+public class Day1 extends Solution2017<List<Integer>>{
+    @Override
+    public int getDay() {
+        return 1;
+    }
+
+    @Override
+    public InputParser<List<Integer>> getInputParser() {
+        return InputParserFactory.getIntegerTokenSVParser("");
+    }
+
+    @Override
+    protected String part1(List<List<Integer>> input) {
+        List<Integer> list = input.get(0);
+        long sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (i == list.size() - 1) {
+                if (list.get(i).equals(list.get(0))) {
+                    sum += list.get(i);
+                }
+            }
+            else if (list.get(i).equals(list.get(i + 1))) {
+                sum += list.get(i);
+            }
+        }
+        return Long.toString(sum);
+    }
+
+    @Override
+    protected String part2(List<List<Integer>> input) {
+        List<Integer> list = input.get(0);
+        long sum = 0;
+        int pair = list.size() / 2;
+        for (int i = 0; i < list.size(); i++) {
+            if (pair == list.size()) {
+                pair = 0;
+            }
+            if (list.get(i).equals(list.get(pair))) {
+                sum += list.get(i);
+            }
+            pair++;
+        }
+        return Long.toString(sum);
+    }
+
+}
---
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2017/Solution2017.java
@@ -0,0 +1,11 @@
+package com.jeffrpowell.adventofcode.aoc2017;
+
+import com.jeffrpowell.adventofcode.Solution;
+
+public abstract class Solution2017<T> extends Solution<T>
+{
+	@Override
+	public int getYear() {
+		return 2017;
+	}
+}
