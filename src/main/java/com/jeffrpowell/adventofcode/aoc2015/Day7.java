d1537a063dfb431c34b92f50f2b39b5de342edb3
Jeff Powell <github@jeffpowell.dev>
2025-12-02 11:36:48 -0700

    2015 day 7

--- src/main/java/com/jeffrpowell/adventofcode/aoc2015/Day7.java
+++ src/main/java/com/jeffrpowell/adventofcode/aoc2015/Day7.java
@@ -1,19 +1,15 @@
 package com.jeffrpowell.adventofcode.aoc2015;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.regex.Pattern;
+
 import com.jeffrpowell.adventofcode.inputparser.InputParser;
 import com.jeffrpowell.adventofcode.inputparser.InputParserFactory;
 import com.jeffrpowell.adventofcode.inputparser.rule.Rule;
 import com.jeffrpowell.adventofcode.inputparser.rule.RuleListUtil;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.Future;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.regex.Pattern;
 
 public class Day7 extends Solution2015<Rule>{
 
@@ -26,12 +22,14 @@ public class Day7 extends Solution2015<Rule>{
     public InputParser<Rule> getInputParser() {
         /*
             123 -> x
+            lx -> a
+            1 AND x -> y
             x AND y -> z
             p LSHIFT 2 -> q
             NOT e -> f
         */
         return InputParserFactory.getRuleParser("\n", Map.of(
-            "input", Pattern.compile("(\\d+) -> (\\w+)"),
+            "input", Pattern.compile("^(\\w+) -> (\\w+)"),
             "comparison", Pattern.compile("(\\w+) (AND|OR) (\\w+) -> (\\w+)"),
             "shift", Pattern.compile("(\\w+) (LSHIFT|RSHIFT) (\\d+) -> (\\w+)"),
             "not", Pattern.compile("NOT (\\w+) -> (\\w+)")
@@ -41,84 +39,153 @@ public class Day7 extends Solution2015<Rule>{
     @Override
     protected String part1(List<Rule> input) {
         Map<String, List<Rule>> groupedRules = RuleListUtil.groupByRulePatternKey(input);
-        Map<String, List<String>> upGraph = new HashMap<>();
-        Map<String, List<String>> downGraph = new HashMap<>();
-        Map<String, BiFunction<Future<Integer>, Future<Integer>, Signal>> m = new HashMap<>();
-        Deque<Signal> q = new ArrayDeque<>();
-        for (Rule rule : groupedRules.get("comparisons")) {
-            upGraph.put(rule.getString(3), List.of(rule.getString(0), rule.getString(2)));
-            downGraph.putIfAbsent(rule.getString(0), new ArrayList<>());
-            downGraph.putIfAbsent(rule.getString(2), new ArrayList<>());
-            downGraph.get(rule.getString(0)).add(rule.getString(3));
-            downGraph.get(rule.getString(2)).add(rule.getString(3));
+        Map<String, Signal> signalMap = new HashMap<>();
+        for (Rule rule : groupedRules.get("comparison")) {
+            if (rule.getString(1).equals("AND")) {
+                signalMap.put(rule.getString(3), Gate.and(rule.getString(0), rule.getString(2)));
+                if (rule.getString(0).matches("\\d+")) {
+                    signalMap.put(rule.getString(0), new StaticSignal(Integer.parseInt(rule.getString(0))));
+                }
+            } else {
+                signalMap.put(rule.getString(3), Gate.or(rule.getString(0), rule.getString(2)));
+            }
         }
         for (Rule rule : groupedRules.get("shift")) {
-            upGraph.put(rule.getString(3), List.of(rule.getString(0)));
-            downGraph.putIfAbsent(rule.getString(0), new ArrayList<>());
-            downGraph.get(rule.getString(0)).add(rule.getString(3));
+            if (rule.getString(1).equals("LSHIFT")) {
+                signalMap.put(rule.getString(3), Gate.lshift(rule.getString(0), rule.getInt(2)));
+            } else {
+                signalMap.put(rule.getString(3), Gate.rshift(rule.getString(0), rule.getInt(2)));
+            }
         }
         for (Rule rule : groupedRules.get("not")) {
-            upGraph.put(rule.getString(1), List.of(rule.getString(0)));
-            downGraph.putIfAbsent(rule.getString(0), new ArrayList<>());
-            downGraph.get(rule.getString(0)).add(rule.getString(1));
+            signalMap.put(rule.getString(1), Gate.not(rule.getString(0)));
         }
         for (Rule rule : groupedRules.get("input")) {
-            upGraph.put(rule.getString(1), List.of());
-            q.add(new Signal(rule.getString(1), rule.getInt(0)));
+            Signal source;
+            if (rule.getString(0).matches("\\d+")) {
+                source = new StaticSignal(Integer.parseInt(rule.getString(0)));
+            }
+            else {
+                source = new WireSignal(rule.getString(0));
+            }
+            signalMap.put(rule.getString(1), source);
         }
-        while (!q.isEmpty()) {
-            Signal s = q.poll();
-            List<String> descendants = downGraph.get(s.wireName);
-        }
-        return "";
+        return Integer.toString(signalMap.get("a").getSignal(signalMap));
     }
 
     @Override
     protected String part2(List<Rule> input) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String part1Result = part1(input);
+        Map<String, List<Rule>> groupedRules = RuleListUtil.groupByRulePatternKey(input);
+        Map<String, Signal> signalMap = new HashMap<>();
+        for (Rule rule : groupedRules.get("comparison")) {
+            if (rule.getString(1).equals("AND")) {
+                signalMap.put(rule.getString(3), Gate.and(rule.getString(0), rule.getString(2)));
+                if (rule.getString(0).matches("\\d+")) {
+                    signalMap.put(rule.getString(0), new StaticSignal(Integer.parseInt(rule.getString(0))));
+                }
+            } else {
+                signalMap.put(rule.getString(3), Gate.or(rule.getString(0), rule.getString(2)));
+            }
+        }
+        for (Rule rule : groupedRules.get("shift")) {
+            if (rule.getString(1).equals("LSHIFT")) {
+                signalMap.put(rule.getString(3), Gate.lshift(rule.getString(0), rule.getInt(2)));
+            } else {
+                signalMap.put(rule.getString(3), Gate.rshift(rule.getString(0), rule.getInt(2)));
+            }
+        }
+        for (Rule rule : groupedRules.get("not")) {
+            signalMap.put(rule.getString(1), Gate.not(rule.getString(0)));
+        }
+        for (Rule rule : groupedRules.get("input")) {
+            Signal source;
+            if (rule.getString(0).matches("\\d+")) {
+                source = new StaticSignal(Integer.parseInt(rule.getString(0)));
+            }
+            else {
+                source = new WireSignal(rule.getString(0));
+            }
+            signalMap.put(rule.getString(1), source);
+        }
+        signalMap.put("b", new StaticSignal(Integer.parseInt(part1Result)));
+        return Integer.toString(signalMap.get("a").getSignal(signalMap));
     }
-    
-    private static class Signal {
-        String wireName;
-        int signal;
 
-        public Signal(String wireName, int signal) {
-            this.wireName = wireName;
+    private static abstract class Signal {
+        private boolean signalCached = false;
+        private int cachedSignal = -1;
+        abstract int loadSignal(Map<String, Signal> signalMap);
+        public int getSignal(Map<String, Signal> signalMap) {
+            if (!signalCached) {
+                int signal = loadSignal(signalMap) & 0xFFFF;
+                signalCached = true;
+                cachedSignal = signal;
+                return signal;
+            } else {
+                return cachedSignal;
+            }
+        }
+    }
+
+    private static class StaticSignal extends Signal {
+        private final int signal;
+        
+        public StaticSignal(int signal) {
             this.signal = signal;
         }
+
+        @Override
+        public int loadSignal(Map<String, Signal> signalMap) {
+            return signal;
+        }
+    }
+
+    private static class WireSignal extends Signal {
+        private final String wireName;
+        
+        public WireSignal(String wireName) {
+            this.wireName = wireName;
+        }
+
+        @Override
+        public int loadSignal(Map<String, Signal> signalMap) {
+            return signalMap.get(wireName).getSignal(signalMap);
+        }
     }
     
-    private static class Gate {
-        private final List<Integer> inputs;
-        private final Function<List<Integer>, Signal> gateFn;
+    private static class Gate extends Signal{
+        private final List<String> sources;
+        private final BiFunction<Map<String, Signal>, List<Signal>, Integer> gateFn;
         
-        public static Gate and(String target) {
-            return new Gate(inputs -> new Signal(target, inputs.get(0) & inputs.get(1)));
+        public static Gate and(String source1, String source2) {
+            return new Gate(List.of(source1, source2), (signalMap, signals) -> (signals.get(0).getSignal(signalMap) & signals.get(1).getSignal(signalMap)) & 0xFFFF);
         }
         
-        public static Gate or(String target) {
-            return new Gate(inputs -> new Signal(target, inputs.get(0) | inputs.get(1)));
+        public static Gate or(String source1, String source2) {
+            return new Gate(List.of(source1, source2), (signalMap, signals) -> (signals.get(0).getSignal(signalMap) | signals.get(1).getSignal(signalMap)) & 0xFFFF);
         }
         
-        public static Gate lshift(String target) {
-            return new Gate(inputs -> new Signal(target, inputs.get(0) << inputs.get(1)));
+        public static Gate lshift(String source, int shift) {
+            return new Gate(List.of(source), (signalMap, signals) -> (signals.get(0).getSignal(signalMap) << shift) & 0xFFFF);
         }
         
-        public static Gate rshift(String target) {
-            return new Gate(inputs -> new Signal(target, inputs.get(0) >> inputs.get(1)));
+        public static Gate rshift(String source, int shift) {
+            return new Gate(List.of(source), (signalMap, signals) -> (signals.get(0).getSignal(signalMap) >> shift) & 0xFFFF);
         }
         
-        public static Gate not(String target) {
-            return new Gate(inputs -> new Signal(target, ~inputs.get(0)));
+        public static Gate not(String source) {
+            return new Gate(List.of(source), (signalMap, signals) -> (~signals.get(0).getSignal(signalMap)) & 0xFFFF);
         }
         
-        private Gate(Function<List<Integer>, Signal> gateFn) {
-            this.inputs = new ArrayList<>();   
+        private Gate(List<String> sources, BiFunction<Map<String, Signal>, List<Signal>, Integer> gateFn) {
+            this.sources = sources;
             this.gateFn = gateFn;
         }
-    
-        public void acceptInput(int input) {
-            inputs.add(input);
+
+        @Override
+        public int loadSignal(Map<String, Signal> signalMap) {
+            return gateFn.apply(signalMap, sources.stream().map(signalMap::get).toList());
         }
     }
 }
